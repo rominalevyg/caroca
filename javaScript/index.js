d808458d3e7e40e8c8e54e16c916792cc79ca1dc
Romina Levy Gagliardini <romy_racing@hotmail.com>
2024-02-11 14:09:51 -0300

    Traducciones

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,6 +2,30 @@ const botonToggle = document.getElementById('hamburguesa');
 const botonClose = document.getElementById('close');
 const divNavegacion = document.getElementById('navegacion');
 
+
+
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+    const requestJson = await fetch(`./languages/${language}.json`);
+    const texts = await requestJson.json();
+    
+    for(const textToChange of textsToChange) {
+        const section=textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+
+        textToChange.innerHTML = texts[section][value];
+    }
+}
+
+flagsElement.addEventListener("click", (e) => {
+    changeLanguage(e.target.parentElement.dataset.language);
+});
+
+
+
 botonToggle.addEventListener('click', () => {
 
     botonToggle.style.display = 'none';
