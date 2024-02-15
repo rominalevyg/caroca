a72ec871e6a746a2839c70b69bfcbea51c9fb5b9
Romina Levy Gagliardini <romy_racing@hotmail.com>
2024-02-15 14:25:47 -0300

    Prueba form

--- php/enviar.php
+++ php/enviar.php
@@ -1,12 +1,11 @@
 <?php
-    $destino = "studiocaroca@gmail.com"
+    $destino = "studiocaroca@gmail.com";
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
     $telefono = $_POST["telefono"];
     $asunto = $_POST["asunto"];
     $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre:" . $nombre . "\nE-mail: " . $email . "\nTel&eacute;fono: " . $telefono . "\nMensaje: " . $mensaje;
+    $contenido = "Nombre: " . $nombre . "\nE-mail: " . $email . "\nTeléfono: " . $telefono . "\nMensaje: " . $mensaje;
     mail($destino, $asunto, $contenido);
     echo "Su mensaje ha sido enviado. Nos comunicaremos a la brevedad. Gracias :)";
-    $('input').val("");
- ?>
+?>
