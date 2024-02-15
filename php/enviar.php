aa50a335388b2eaaa7ab9bb0b106d05ec1d4476b
Romina Levy Gagliardini <romy_racing@hotmail.com>
2024-02-15 16:38:53 -0300

    Prueba js

--- php/enviar.php
+++ php/enviar.php
@@ -1,4 +1,5 @@
 <?php
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
     $destino = "studiocaroca@gmail.com"
     $nombre = $_POST["nombre"];
     $email = $_POST["email"];
@@ -7,6 +8,7 @@
     $mensaje = $_POST["mensaje"];
     $contenido = "Nombre:" . $nombre . "\nE-mail: " . $email . "\nTel&eacute;fono: " . $telefono . "\nMensaje: " . $mensaje;
     mail($destino, $asunto, $contenido);
-    echo "Su mensaje ha sido enviado. Nos comunicaremos a la brevedad. Gracias :)";
+    // echo "Su mensaje ha sido enviado. Nos comunicaremos a la brevedad. Gracias :)";
     $('input').val("");
+}
  ?>
